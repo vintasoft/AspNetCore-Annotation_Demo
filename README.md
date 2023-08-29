147e4cb5bcc81fa508ccd89a9bb58bb7797a12bc
vintasoft <alex@vintasoft.com>
2023-08-29 10:58:22 +0300

    Source codes update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# AspNetCore-Annotation_Demo
-VintaSoft ASP.NET Core Annotation Demo
+# VintaSoft ASP.NET Core Annotation Demo
+
+This ASP.NET Core project uses <a href="https://www.vintasoft.com/vsimaging-dotnet-index.html">VintaSoft Imaging .NET SDK</a> and demonstrates how to view and annotate images and documents in ASP.NET Core.
+
+
+## Screenshot
+<img src="vintasoft-aspnet.core-annotation-demo.png" alt="VintaSoft ASP.NET Core Annotation Demo">
+
+
+## Usage
+1. Get the 30 day free evaluation license for <a href="https://www.vintasoft.com/vsimaging-dotnet-index.html" target="_blank">VintaSoft Imaging .NET SDK</a> as described here: <a href="https://www.vintasoft.com/docs/vsimaging-dotnet/Licensing-Evaluation.html" target="_blank">https://www.vintasoft.com/docs/vsimaging-dotnet/Licensing-Evaluation.html</a>
+
+2. Update the evaluation license in "src\Startup.cs" file:
+   ```
+   Vintasoft.Imaging.ImagingGlobalSettings.Register("REG_USER", "REG_EMAIL", "EXPIRATION_DATE", "REG_CODE");
+   ```
+
+3. Build the project ("AspNetCoreAnnotationDemo.Net7.csproj" file) in Visual Studio or using .NET CLI:
+   ```
+   dotnet build AspNetCoreAnnotationDemo.Net7.csproj
+   ```
+
+4. Run compiled application and try to view and annotate images and documents.
+
+
+## Documentation
+VintaSoft Imaging .NET SDK on-line User Guide and API Reference for .NET developer is available here: https://www.vintasoft.com/docs/vsimaging-dotnet/
+
+
+## Support
+Please visit our <a href="https://myaccount.vintasoft.com/">online support center</a> if you have any question or problem.
---
+++ src/wwwroot/js/SaveAnnotationsAndDownloadFileHelper.js
@@ -0,0 +1,146 @@
+﻿/**
+ A helper that helps to save annotations to a file and download file with annotations.
+*/
+SaveAnnotationsAndDownloadFileHelperJS = function (showErrorMessageFunc) {
+
+    var _docViewer;
+    var _imageCount;
+    var _savedImageCount;
+
+
+
+    /**
+     Creates UI button that allows to download a file with annotations.
+    */
+    SaveAnnotationsAndDownloadFileHelperJS.prototype.createDownloadFileWithAnnotationsButton = function () {
+        // create button that allows to download image file
+        var element = new Vintasoft.Imaging.UI.UIElements.WebUiButtonJS({
+            cssClass: "vsdv-downloadImageFileButton",
+            title: "Download Image File",
+            localizationId: "downloadImageButton",
+            onClick: __downloadImageFileButton_clicked
+        });
+        return element;
+    }
+
+    function __downloadImageFileButton_clicked(event, uiElement) {
+        _docViewer = uiElement.get_RootControl();
+        // get image viewer
+        var imageViewer = _docViewer.get_ImageViewer();
+        // get images of image viewer
+        var images = imageViewer.get_Images();
+
+        // get image count
+        _imageCount = images.get_Count();
+        _savedImageCount = 0;
+        // if image viewer contains images
+        if (_imageCount != 0) {
+            // get annotation controller
+            var controller = imageViewer.get_AnnotationController();
+            // start the asynchronous saving of annotation collections
+            controller.saveAnnotationCollections(__saveAnnotationCollections_success, __downloadFile_error);
+
+            // start the asynchronous operation in document viewer
+            _docViewer.startAsyncOperation("Download file");
+        }
+    }
+
+    /**
+     Saves blob to a file.
+     @param {object} blob Blob.
+     @param {string} filename File name.
+    */
+    function __saveBlobToFile(blob, filename) {
+        // if blob is defined
+        if (blob != null) {
+            // if web browser can save blobs
+            if (navigator.msSaveBlob) {
+                // save the blob using web browser functionality
+                return navigator.msSaveBlob(blob, filename);
+            }
+            // if web browser CANNOT save blobs
+            else {
+                // create an object URL
+                var url = window.URL.createObjectURL(blob);
+                // if object URL is created
+                if (url != null) {
+                    // create an "A" element
+                    var a = document.createElement("a");
+                    a.style.display = "none";
+                    // if "A" element supports the "download" attribute
+                    if ("download" in a) {
+                        // create "A" element with "download" attribute for saving a file in browser
+
+                        a.setAttribute("href", url);
+                        a.setAttribute("download", filename);
+                        document.body.appendChild(a);
+
+                        setTimeout(function () {
+                            a.click();
+                            a.remove();
+                            setTimeout(function () { window.URL.revokeObjectURL(url); }, 250);
+                        }, 66);
+                    }
+                    // if "A" element does NOT support the "download" attribute
+                    else {
+                        // create iframe for saving a file in browser
+                        var frame = document.createElement("iframe");
+                        document.body.appendChild(a);
+                        frame[0].src = url;
+
+                        setTimeout(function () { frame.remove(); }, 333);
+                    }
+                }
+                // if object URL is NOT created
+                else {
+                    // show the alert if warning occured
+                    showErrorMessageFunc("Error: Object URL is not created.");
+                }
+            }
+        }
+        // if blob is NOT created
+        else {
+            // show the alert if warning occured
+            showErrorMessageFunc("Error: Blob is not created.");
+        }
+    }
+
+    /**
+     The request for downloading image file from server is executed successfully.
+    */
+    function __onDownloadFile_success(data) {
+        // get a blob, which contains data of downloading file
+        var blob = data.blob;
+        // get name of downloading file
+        var filename = data.filename;
+        __saveBlobToFile(blob, filename);
+
+        // stop the asynchronous operation in document viewer
+        _docViewer.finishAsyncOperation("Download file", data);
+    }
+
+    /**
+     Request for saving annotation collections in image file is executed successfully.
+    */
+    function __saveAnnotationCollections_success(data) {
+        // get image viewer
+        var imageViewer = _docViewer.get_ImageViewer();
+        // get images of image viewer
+        var images = imageViewer.get_Images();
+
+        _savedImageCount++;
+        if (_savedImageCount === _imageCount) {
+            var image = images.getImage(0);
+            var source = image.get_Source();
+            Vintasoft.Imaging.VintasoftFileAPI.downloadImageFile(source, __onDownloadFile_success, __downloadFile_error);
+        }
+    }
+
+    /**
+     Download file operation is failed.
+    */
+    function __downloadFile_error(data) {
+        _docViewer.failAsyncOperation("Download file", data);
+    }
+
+}
